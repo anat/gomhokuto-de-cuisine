b98d3d6b12547867be4a2a08c0c63e2640a97e5a
ancelf <ancelf@localhost>
2011-05-09 23:44:34 +0200

    Il manque les regle optionnel speciale qui font chier

--- inc/Referee.hpp
+++ inc/Referee.hpp
@@ -1,9 +1,9 @@
 /* 
- * File:   Referee.hpp
- * Author: fancel
- *
- * Created on May 5, 2011, 1:14 PM
- */
+* File:   Referee.hpp
+* Author: fancel
+*
+* Created on May 5, 2011, 1:14 PM
+*/
 
 #ifndef REFEREE_HPP
 #define	REFEREE_HPP
@@ -16,27 +16,50 @@
 #define DIR_RIGHT 0x8
 
 /*
- * L'arbitre 
- */
+* L'arbitre 
+*/
 
 class Referee {
 public:
-    Referee(Board& board);
-    Referee(const Referee& orig);
-    virtual ~Referee();
+	Referee(Board& board);
+	Referee(const Referee& orig);
+	virtual ~Referee();
 
 	int tryPlaceRock(unsigned int x, unsigned int y, Square::Player& square); //Test et si ok place
-    bool testPosition(unsigned int x, unsigned int y); //Test tout cours
-	void checkAlignement(unsigned int x, unsigned int y);
-	bool checkPosition(unsigned int x, unsigned int y);
-	void propagation(unsigned int x, unsigned int y, const Square::Player& player);
-        void propagation(unsigned int x, unsigned int y, const Square::Player& player,
-                         unsigned int dir, unsigned int usize);
+	bool testPosition(unsigned int x, unsigned int y, Square::Player& player); //Test tout cours
+	const Square::Player& checkWin() const;
 	void propagation_inverse(unsigned int x, unsigned int y, const Square::Player& player);
-        int lineSize(unsigned int x, unsigned int y, const Square::Player& player, int dir);
-	void checkPrise(unsigned int x, unsigned int y, const Square::Player& player);
+
 private:
-    Board& _board;
+	bool _fivePrize;
+	bool _doubleThree;
+	Square::Player _winner;
+	Board& _board;
+
+	inline Square::Player opponant(const Square::Player& pla) {
+		Square::Player result = Square::NOPLAYER;
+		if (pla == Square::PLAYER1)
+			result = Square::PLAYER2;
+		else if (pla == Square::PLAYER2)
+			result = Square::PLAYER1;
+		return result;
+	}
+
+	inline bool checkPosition(unsigned int x, unsigned int y) {
+		return (x < _board.getSize() && y < _board.getSize());
+	}
+
+	bool checkDoubleThree(unsigned int x, unsigned int y, Square::Player& player);
+	bool checkFivePrize(unsigned int x, unsigned int y, Square::Player& player);
+	void checkWin(unsigned int x, unsigned int y, Square::Player& player);
+	unsigned int checkPrize(unsigned int x, unsigned int y, const Square::Player& player);
+	bool checkPrize(unsigned int x, unsigned int y, int xvec, int yvec, const Square::Player& play);
+	void cleanRock(unsigned int x, unsigned int y, int xvec, int yvec, const Square::Player& play);
+	void propagation(unsigned int x, unsigned int y, const Square::Player& player);
+	void propagation(unsigned int x, unsigned int y, const Square::Player& player,
+		unsigned int dir, unsigned int usize);
+	
+	int lineSize(unsigned int x, unsigned int y, const Square::Player& player, int dir);
 };
 
 #endif	/* REFEREE_HPP */
