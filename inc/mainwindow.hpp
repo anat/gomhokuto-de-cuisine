dc5b0d9382e8b0a5315a2c13cd8cb44017e6fa1c
Anatole Korczak <anat@users.noreply.github.com>
2011-05-15 18:47:21 +0200

    Suppression de l'ancien GUI, merge de l'interface graphique avec le gomoku, vous devez utiliser l'option iaonly comme ceci ./Gomoku iaonly si vous voulez retrouver le gomoku classique, l'interface n'a pas avance mais ne vous inquietez pas on arrive,
    Voila j'peux aussi raconter ma vie dans ce message de commit,
    
    Bonne soiree

---
+++ inc/mainwindow.hpp
@@ -0,0 +1,30 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtGui>
+
+namespace Ui {
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+    Ui::MainWindow  *ui;
+    QGraphicsScene  *scene;
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    void DrawCadre();
+
+
+protected:
+    void resizeEvent(QResizeEvent *event);
+
+};
+
+#endif // MAINWINDOW_H
