467959e3aacb6de3709843cfeb6978435bf8021d
Fcx <Fcx@localhost>
2011-05-16 20:26:45 +0200

    interface

--- inc/mainwindow.hpp
+++ inc/mainwindow.hpp
@@ -14,21 +14,29 @@ class MainWindow : public QMainWindow
 
     Ui::MainWindow  *ui;
     QGraphicsScene  *scene;
+    int             border;
+    float           refh;
+    float           refw;
+
+    void DrawCadre(QColor &color);
+    void Drawrepere(QColor &color, int x, int y);
 
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-    void DrawCadre(QColor &color);
-    void Drawrepere(QColor &color, int x, int y);
+    void DrawPiece(int x, int y);
 
 protected:
     void resizeEvent(QResizeEvent *event);
+    void mousePressEvent(QMouseEvent *event);
 
 public slots:
-void print_status();
-
+    void print_status();
+    void print_circle();
 
 };
 
 #endif // MAINWINDOW_H
+
+
