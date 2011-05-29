e9cf79b77fe67ec5134e57ca2661bca8fb439cfe
Chanioux Florian <Chanioux Florian@localhost>
2011-05-29 02:45:23 +0200

    gestion de l'affichage de la board.
    # Bug non resolut et insolvable. du au fait que QT garde un espace pour la barre de menu.
    hors, il n'y a pas de barre de menu sur mac. d'ou l'obligation d'effectuer un resize.

--- inc/mainwindow.hpp
+++ inc/mainwindow.hpp
@@ -21,11 +21,19 @@ class MainWindow : public QMainWindow
     int              _border;
     float            _refh;
     float            _refw;
+    float            _heightWB;
+    float            _widthWB;
+    float            _height;
+    float            _width;
+
+    void DrawScene();
+
+    void InfoDraw();
 
     void DrawShelf(QColor &color);
     void DrawMark(QColor &color, int x, int y);
-    void DrawPiece(int x, int y);
     void DrawBoard();
+    void DrawPiece(int x, int y);
 
 public:
     explicit MainWindow(QWidget *parent = 0);
@@ -39,7 +47,12 @@ protected:
 public slots:
     void print_status();
     void print_circle();
+    void DrawAll();
+    void trytopose(int x, int y);
 
+signals:
+    void SignalPosMouse(int x, int y);
+    void ReadyToDraw();
 };
 
 #endif // MAINWINDOW_H
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -7,41 +7,70 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     _ui(new Ui::MainWindow)
 {
-    _game = new Game();
-    _sizeboard = 19;
     _ui->setupUi(this);
+    _game = new Game();
+    _scene = NULL;
+    _sizeboard = 19;
+    _border = 25;
+
     QObject::connect(_ui->actionNew, SIGNAL(triggered()), this, SLOT(print_status()));
     QObject::connect(_ui->actionParameters, SIGNAL(triggered()), this, SLOT(print_circle()));
+    QObject::connect(this, SIGNAL(ReadyToDraw()), this, SLOT(DrawAll()));
+    QObject::connect(this, SIGNAL(SignalPosMouse(int,int)), this, SLOT(trytopose(int,int)));
 }
 
 MainWindow::~MainWindow()
 {
     delete _ui;
+    delete _game;
     delete _scene;
 }
 
-void MainWindow::DrawShelf(QColor &color)
+void MainWindow::InfoDraw()
 {
+
+    float size = (float) _sizeboard;
+
+    _height = (float)_ui->WGameBoard->height();
+    _width = (float)_ui->WGameBoard->width();
+    _heightWB = _height - 2 * _border;
+    _widthWB = _width - 2 * _border;
+    _refh = _heightWB / (size - 1);
+    _refw = _widthWB / (size - 1);
+    std::cout << "_height= " << _height << std::endl <<
+                 "_width= " << _width << std::endl <<
+                 "_border= " << _border << std::endl <<
+                 "_refh= " << _refh << std::endl <<
+                 "_refw= " << _refw << std::endl <<
+                 "_heightWB= " << _heightWB << std::endl <<
+                 "_widthWB= " << _widthWB << std::endl;
+
+}
+
+void MainWindow::DrawScene()
+{
+    if (_scene != NULL)
+        delete _scene;
+    _scene = new QGraphicsScene(0, 0,
+                                _width,
+                                _height);
+    _ui->GameBoard->setScene(_scene);
+}
+
+void MainWindow::DrawShelf(QColor &color)
+{ 
     QImage *image = new QImage(":/Background/parquet.jpg");
     QBrush *brush = new QBrush(*image);
-    _scene->setBackgroundBrush(*brush);
+    _ui->GameBoard->scene()->setBackgroundBrush(*brush);
     QPen pen(color, 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
-    float size = (float) _sizeboard;
-    _border = 25;
-    float height = _ui->gameboard->geometry().height();
-    float width = _ui->gameboard->geometry().width();
-    float heightb = height - 2 * _border;
-    float widthb = width - 2 * _border;
-    _refh = heightb / (size - 1);
-    _refw = widthb / (size - 1);
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < _sizeboard; i++)
     {
         QLine v_line(_border + i * _refw, _border,
-                     _border + i * _refw, heightb + _border);
+                     _border + i * _refw, _heightWB + _border);
         QLine h_line(_border, _border + i * _refh,
-                     widthb + _border, _border + i * _refh);
-        _ui->gameboard->scene()->addLine(h_line, pen);
-        _ui->gameboard->scene()->addLine(v_line, pen);
+                     _widthWB + _border, _border + i * _refh);
+        _ui->GameBoard->scene()->addLine(h_line, pen);
+        _ui->GameBoard->scene()->addLine(v_line, pen);
     }
     DrawMark(color, _border + 3 * _refw, _border + 3 * _refh);
     DrawMark(color, _border + 3 * _refw, _border + 9 * _refh);
@@ -52,22 +81,21 @@ void MainWindow::DrawShelf(QColor &color)
     DrawMark(color, _border + 15 * _refw, _border + 3 * _refh);
     DrawMark(color, _border + 15 * _refw, _border + 9 * _refh);
     DrawMark(color, _border + 15 * _refw, _border + 15 * _refh);
-
 }
 
 void MainWindow::DrawMark(QColor &color, int x, int y)
 {
+
     QPen pen(color, 1, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
-    QGraphicsScene *canvas = _ui->gameboard->scene();
-    QAbstractGraphicsShapeItem* i = canvas->addEllipse(QRectF(0,0,8,8), pen);
+    QAbstractGraphicsShapeItem* i = _ui->GameBoard->scene()->addEllipse(QRectF(0,0,8,8), pen);
     i->setBrush(color);
     i->setPos(x - 4, y - 4);
 }
 
 void MainWindow::DrawPiece(int x, int y)
 {
-    QGraphicsScene *canvas = _ui->gameboard->scene();
-    QAbstractGraphicsShapeItem* i = canvas->addEllipse(QRectF(0, 0, 30, 30));
+    std::cout << "DrawPiece enter" << std::endl;
+    QAbstractGraphicsShapeItem* i = _ui->GameBoard->scene()->addEllipse(QRectF(0, 0, 30, 30));
     //i->setFlag(QGraphicsItem::ItemIsMovable);
     i->setPen(Qt::NoPen);
     i->setBrush( QColor(qrand()%32*8,qrand()%32*8,qrand()%32*8) );
@@ -76,46 +104,68 @@ void MainWindow::DrawPiece(int x, int y)
 
 void MainWindow::DrawBoard()
 {
-    // clear scene
-    // parcours de _game->getgameBoard() en x, y
-    // en fonction du pion,
-    // Drawpiece(x, y)
-    // emit signal si possible.
+    for (int x = 0; x < this->_sizeboard; x++)
+        for (int y = 0; y < this->_sizeboard; y++)
+        {
+            switch (this->_game->getGameBoard().getCase(x, y).getData().player)
+            {
+            case PLAYER1:
+                this->DrawPiece(x, y);
+                break;
+            case PLAYER2:
+                this->DrawPiece(x, y);
+                break;
+            default:
+                break;
+            }
+        }
 }
 
 void MainWindow::resizeEvent(QResizeEvent *event)
 {
-    _scene = new QGraphicsScene(5, 5,
-                                _ui->gameboard->geometry().width() - 5,
-                                _ui->gameboard->geometry().height() - 5);
-    _ui->gameboard->setScene(_scene);
-    _ui->gameboard->scene()->clear();
-    QColor color(Qt::black);
-    DrawShelf(color);
+    event = event;
+    std::cout << "signal emit: SignalResize()" << std::endl;
+    this->InfoDraw();
     update();
-    QWidget::resizeEvent(event);
+    this->DrawScene();
+    emit ReadyToDraw();
 }
 
 void MainWindow::mousePressEvent(QMouseEvent *event)
 {
-    float casex = (event->pos().x() - _border) / _refw;
-    float casey = (event->pos().y() - _border) / _refh;
 
-    std::cout << "casex = " << casex << std::endl <<
-                 "casey = " << casey << std::endl;
-    std::cout << "cadre1 = " << ((casex * _refw + _border) - 10 ) << std::endl <<
-                 "cadre2 = " << ((casex * _refw + _border) + 10 ) << std::endl;
-    if ((event->pos().x() > (casex * _refw + _border) - 10 ) &&
-        (event->pos().x() < (casex * _refw + _border) + 10 ))
-       this->DrawPiece((casex * _refw + _border),
-                       (event->pos().y()));
-
-    std::cout << "x = " << event->pos().x() << std::endl <<
-                 "y = " << event->pos().y() << std::endl <<
-                 "height= " << _ui->gameboard->geometry().height() << std::endl <<
-                 "width= " << _ui->gameboard->geometry().width() << std::endl <<
+    int x, y, nx = -1, ny = -1;
+    for (x = 0; x < _sizeboard; x++)
+    {
+        if ((event->pos().x() > (float)x * _refw + _border - _refw / 2) &&
+            (event->pos().x() < (float)x * _refw + _border + _refw / 2))
+        {
+            nx = x;
+            break;
+        }
+    }
+    for (y = 0; y < _sizeboard; y++)
+    {
+        if ((event->pos().y() > (float)y * _refh + _border - _refh / 2) &&
+            (event->pos().y() < (float)y * _refh + _border + _refh / 2))
+        {
+            ny = y;
+            break;
+        }
+    }
+    std::cout << "_height= " << _height << std::endl <<
+                 "_width= " << _width << std::endl <<
+                 "_border= " << _border << std::endl <<
                  "_refh= " << _refh << std::endl <<
-                 "_refw= " << _refw << std::endl;
+                 "_refw= " << _refw << std::endl <<
+                 "_heightWB= " << _heightWB << std::endl <<
+                 "_widthWB= " << _widthWB << std::endl <<
+                 "x = " << event->pos().x() << std::endl <<
+                 "y = " << event->pos().y() << std::endl <<
+                 "my new x= " << nx << std::endl <<
+                 "my new y= " << ny << std::endl;
+    if (ny != -1 && nx != -1)
+        emit SignalPosMouse(nx, ny);
 }
 
 void MainWindow::print_status()
@@ -126,3 +176,19 @@ void MainWindow::print_status()
 void MainWindow::print_circle()
 {
 }
+
+void MainWindow::DrawAll()
+{
+    QColor color(Qt::black);
+
+    _ui->GameBoard->scene()->clear();
+    this->DrawShelf(color);
+    this->DrawBoard();
+}
+
+void MainWindow::trytopose(int x, int y)
+{
+    // inserer dans le tableau, une piece a la position x,y
+    emit ReadyToDraw();
+}
+
